0959c51633744ce990a27253f6582dd64d9374e2
minchanjun <minchanjun@gmail.com>
2018-03-18 10:58:35 +0900

    User Model created and relationship added to Category and CategoryItem model

--- vagrant/catalog/models.py
+++ vagrant/catalog/models.py
@@ -7,12 +7,23 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+## Update User Model
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable = False)
+    email = Column(String(250), nullable = False)
+    picture = Column(String(250))
+
 ####
 class Category(Base):
     __tablename__ = 'category'
 
     id = Column(Integer, primary_key = True)
     name = Column(String(250), nullable = False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -31,6 +42,8 @@ class CategoryItem(Base):
     description = Column(String(250))
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -42,6 +55,6 @@ class CategoryItem(Base):
             'name': self.name,
             'description': self.description,
         }
-engine = create_engine('sqlite:///itemcatalogapp.db')
+engine = create_engine('sqlite:///itemcatalogapp_copy.db')
 
 Base.metadata.create_all(engine)
